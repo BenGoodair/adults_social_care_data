e4ba10ed36c749dca69e317bdc06d12b73d953ad
Ben Goodair <63054419+BenGoodair@users.noreply.github.com>
2023-11-29 15:30:17 +0000

    when I grow up I wanna be an MC

---
+++ Code/activity_cleaning_code.R
@@ -0,0 +1,18 @@
+if (!require("pacman")) install.packages("pacman")
+
+pacman::p_load(devtools, dplyr, tidyverse, tidyr, stringr,  curl)
+
+
+
+rm(list=setdiff(ls(), c("")))
+
+
+options(scipen=999)
+
+
+setwd("C:/Users/benjamin.goodair/OneDrive - Nexus365/Documents/Children's Care Homes Project")
+
+####ASC-FR####
+
+
+
