242cb59095c11938d3cac176936d3a9b54486b36
Ben Goodair <63054419+BenGoodair@users.noreply.github.com>
2024-01-09 10:37:18 +0000

    Hang the DJ

--- Code/activity_cleaning_code.R
+++ Code/activity_cleaning_code.R
@@ -34,10 +34,13 @@ asc19 <- read.csv(curl("https://raw.githubusercontent.com/BenGoodair/adults_soci
 asc18 <- read.csv(curl("https://raw.githubusercontent.com/BenGoodair/adults_social_care_data/main/Raw_data/Copy%20of%20ASCFR%20Data%20File%20(with%20descriptions)_2018.csv"))
 
 
-asc17 <- read.csv(curl())
-asc16 <- read.csv(curl())
-asc15 <- read.csv(curl())
-asc14 <- read.csv(curl())
+asc17 <- read.csv(curl("https://raw.githubusercontent.com/BenGoodair/adults_social_care_data/main/Raw_data/2017/CSV%20ASCFR%20Activity.csv"))
+asc16 <- read.csv(curl("https://raw.githubusercontent.com/BenGoodair/adults_social_care_data/main/Raw_data/2016/pss-exp-eng-15-16-fin-act.csv"))
+asc15 <- read.csv(curl("https://raw.githubusercontent.com/BenGoodair/adults_social_care_data/main/Raw_data/2015/pss-exp-eng-14-15-fin-coun-lev-act-data.csv"), skip=1)
+
+
+
+asc14 <- read.csv(curl("https://raw.githubusercontent.com/BenGoodair/adults_social_care_data/main/Raw_data/2014/Annex_Final_CASSR_Level_Activity_Data_2013_14.csv"))
 asc13 <- read.csv(curl())
 asc12 <- read.csv(curl())
 asc11 <- read.csv(curl())
@@ -114,4 +117,132 @@ asc18 <- asc18 %>% dplyr::filter(GEOGRAPHY_LEVEL=="Local Authority",
   dplyr::group_by(DH_GEOGRAPHY_NAME, GEOGRAPHY_CODE, SupportSetting_Key) %>%
   dplyr::mutate(percent_sector = ITEMVALUE /ITEMVALUE[ActivityProvision_Key == "99"]*100) %>%
   dplyr::ungroup()
+
+total17 <- asc17 %>% dplyr::filter(GEOGRAPHY_CODE!="")%>%
+  dplyr::mutate(ItemValue=as.numeric(ItemValue))%>%
+  dplyr::select(GEOGRAPHY_CODE, SupportSetting_KEY, ItemValue)%>%
+  dplyr::group_by(GEOGRAPHY_CODE, SupportSetting_KEY) %>%
+  dplyr::summarise(ItemValue = sum(ItemValue, na.rm=T))%>%
+  dplyr::ungroup()%>%
+  dplyr::mutate(ActivityProvision_KEY=1)
+
+
+ asc17 <- asc17 %>% dplyr::filter(GEOGRAPHY_CODE!="")%>%
+   dplyr::mutate( ItemValue=as.numeric(ItemValue))%>%
+   dplyr::select(GEOGRAPHY_CODE, SupportSetting_KEY, ActivityProvision_KEY, ItemValue)%>%
+   dplyr::bind_rows(., total17)%>%
+   dplyr::mutate(ActivityProvision_KEY = ifelse(ActivityProvision_KEY==1,"99",
+                                                         ifelse(ActivityProvision_KEY==2,"External",
+                                                                ifelse(ActivityProvision_KEY==3,"In House",NA))),
+                          SupportSetting_KEY = ifelse(SupportSetting_KEY==6, "Nursing",
+                                                      ifelse(SupportSetting_KEY == 7,"Residential",
+                                                             ifelse(is.na(SupportSetting_KEY), "", NA))))%>%
+  dplyr::rename(SupportSetting=SupportSetting_KEY,
+                ActivityProvision=ActivityProvision_KEY,
+                ITEMVALUE = ItemValue)%>%
+  dplyr::group_by(GEOGRAPHY_CODE, ActivityProvision, SupportSetting) %>%
+  dplyr::summarise(ITEMVALUE = sum(ITEMVALUE, na.rm=T))%>%
+  dplyr::ungroup()%>%
+  dplyr::group_by(GEOGRAPHY_CODE, SupportSetting) %>%
+   dplyr::mutate(percent_sector = ITEMVALUE /ITEMVALUE[ActivityProvision == "99"]*100) %>%
+   dplyr::ungroup()%>%
+  dplyr::mutate(year=2017,
+                DH_GEOGRAPHY_NAME=NA)%>%
+   dplyr::mutate(SupportSetting = ifelse(is.na(SupportSetting), "", SupportSetting))
+  
+ 
+
+ total16 <- asc16 %>% dplyr::filter(Council.Code!="")%>%
+   dplyr::mutate(Value=as.numeric(Value))%>%
+   dplyr::select(Council.Code, Council.Name, Activity.Provision, Long.Term.Support.Setting, Value)%>%
+   dplyr::group_by(Council.Code, Council.Name, Long.Term.Support.Setting) %>%
+   dplyr::summarise(Value = sum(Value, na.rm  =T))%>%
+   dplyr::ungroup()%>%
+   dplyr::mutate(Activity.Provision="99")
+ 
+ 
+ 
+ asc16 <- asc16 %>% dplyr::filter(Council.Code!="")%>%
+   dplyr::mutate( Value=as.numeric(Value))%>%
+   dplyr::select(Council.Code, Council.Name, Long.Term.Support.Setting, Activity.Provision, Value)%>%
+   dplyr::bind_rows(., total16)%>%
+   dplyr::rename(SupportSetting=Long.Term.Support.Setting,
+                 ActivityProvision=Activity.Provision,
+                 ITEMVALUE = Value,
+                 GEOGRAPHY_CODE = Council.Code,
+                 DH_GEOGRAPHY_NAME = Council.Name)%>%
+   dplyr::group_by(GEOGRAPHY_CODE,DH_GEOGRAPHY_NAME, ActivityProvision, SupportSetting) %>%
+   dplyr::summarise(ITEMVALUE = sum(ITEMVALUE, na.rm=T))%>%
+   dplyr::ungroup()%>%
+   dplyr::group_by(GEOGRAPHY_CODE, DH_GEOGRAPHY_NAME,SupportSetting) %>%
+   dplyr::mutate(percent_sector = ITEMVALUE /ITEMVALUE[ActivityProvision == "99"]*100,
+                 SupportSetting = ifelse(SupportSetting=="-", "", SupportSetting)) %>%
+   dplyr::ungroup()%>%
+   dplyr::mutate(year=2016)%>%
+   dplyr::mutate(SupportSetting = ifelse(is.na(SupportSetting), "", SupportSetting))
+ 
+asc15 <- asc15 %>%
+  pivot_longer( cols=!c(Support.type, Age.category, Primary.support.reason, Service.delivery.mechanism, Provision), 
+               names_to = "DH_GEOGRAPHY_NAME", values_to = "ITEMVALUE", names_prefix = "...")
+ 
+
+plotfun <- rbind(asc18[c("percent_sector", "SupportSetting_Key", "DH_GEOGRAPHY_NAME", "ActivityProvision_Key")]%>%
+                   dplyr::rename(SupportSetting=SupportSetting_Key,
+                                 ActivityProvision=ActivityProvision_Key)%>%
+                   dplyr::mutate(year=2018),
+                 asc19[c("percent_sector", "SupportSetting_Key", "DH_GEOGRAPHY_NAME", "ActivityProvision_Key")]%>%
+                   dplyr::rename(SupportSetting=SupportSetting_Key,
+                                 ActivityProvision=ActivityProvision_Key)%>%
+                   dplyr::mutate(year=2019),
+                 asc20[c("percent_sector", "SupportSetting", "DH_GEOGRAPHY_NAME", "ActivityProvision")]%>%
+                   dplyr::mutate(year=2020),
+                 asc21[c("percent_sector", "SupportSetting", "DH_GEOGRAPHY_NAME", "ActivityProvision")]%>%
+                   dplyr::mutate(year=2021),
+                 asc22[c("percent_sector", "SupportSetting", "DH_GEOGRAPHY_NAME", "ActivityProvision")]%>%
+                   dplyr::mutate(year=2022),
+                 asc23[c("percent_sector", "SupportSetting", "DH_GEOGRAPHY_NAME", "ActivityProvision")]%>%
+                   dplyr::mutate(year=2023)
+                 )%>%
+  dplyr::filter(ActivityProvision=="External")
+
+library(ggplot2)
+
+# Assuming 'filtered_df_spend' is your data frame in R
+
+one <- ggplot(plotfun[plotfun$SupportSetting=="Nursing",], aes(x = year, y = percent_sector, color = percent_sector)) +
+  geom_point(size = 3) +
+  geom_smooth(method = "loess", se = FALSE) +
+  labs(
+    x = "Year",
+    y = "Outsourced activity (%)",
+    title = "Percent of activity outsourced by LAs\nNursing Care",
+    color = "Outsourced %"
+  )+
+  theme_bw()
+
+
+two <- ggplot(plotfun[plotfun$SupportSetting=="Residential",], aes(x = year, y = percent_sector, color = percent_sector)) +
+  geom_point(size = 3) +
+  geom_smooth(method = "loess", se = FALSE) +
+  labs(
+    x = "Year",
+    y = "Outsourced activity (%)",
+    title = "Residential Care",
+    color = "Outsourced %"
+  )+
+  theme_bw()
+
+three <- ggplot(plotfun[plotfun$SupportSetting=="",], aes(x = year, y = percent_sector, color = percent_sector)) +
+  geom_point(size = 3) +
+  geom_smooth(method = "loess", se = FALSE) +
+  labs(
+    x = "Year",
+    y = "Outsourced activity (%)",
+    title = "Other Care",
+    color = "Outsourced %"
+  )+
+  theme_bw()
+
+
+plot <- cowplot::plot_grid(one,two,three, ncol=1)
 
