42ab5bb73d2ce4d62625df9b64734d73f36f31c3
Ben Goodair <63054419+BenGoodair@users.noreply.github.com>
2024-02-07 16:31:04 +0000

    I never ever call and ask you to play something right?

--- Code/spending_cleaning_code.R
+++ Code/spending_cleaning_code.R
@@ -39,6 +39,109 @@ pacman::p_load(devtools, dplyr, tidyverse, tidyr, stringr,  curl, plm, readxl)
 
 
 
+#2001
+ 
+ # excel_path <- "C:\\Users\\benjamin.goodair\\OneDrive - Nexus365\\Documents\\GitHub\\adults_social_care_data\\Raw_data\\2001\\spend.xls"
+ # 
+ # excel_sheets <- excel_sheets(excel_path)
+ # 
+ # for (sheet_name in excel_sheets) {
+ #   df <- read_excel(excel_path, sheet = sheet_name, col_types = NULL)
+ #   
+ #   csv_path <- paste0("C:\\Users\\benjamin.goodair\\OneDrive - Nexus365\\Documents\\GitHub\\adults_social_care_data\\Raw_data\\2001\\spend_csvs\\output_", sheet_name, ".csv")
+ #   
+ #   write.csv(df, file = csv_path, row.names = FALSE)
+ # }
+ # 
+ # write.csv(excel_sheets, file = "C:\\Users\\benjamin.goodair\\OneDrive - Nexus365\\Documents\\GitHub\\adults_social_care_data\\Raw_data\\2001\\spend_csvs\\sheet_names.csv", row.names = FALSE)
+ # 
+ # sheet_names <- read.csv(curl("https://raw.githubusercontent.com/BenGoodair/adults_social_care_data/main/Raw_data/2006/council_csvs/sheet_names.csv"))
+ # 
+ # sheet_names$x <- gsub(" ", "%20", sheet_names$x)
+ # 
+
+ all_df <- read.csv(curl("https://raw.githubusercontent.com/BenGoodair/adults_social_care_data/main/Raw_data/2006/council_csvs/output_Barnsley.csv"), skip=1)%>%
+   dplyr::select(dplyr::starts_with("Data"))%>%
+   dplyr::mutate(DH_GEOGRAPHY_NAME=sheet_names$x[1])
+ 
+ 
+ for (i in sheet_names$x) {
+   df <- read.csv(curl(paste0("https://raw.githubusercontent.com/BenGoodair/adults_social_care_data/main/Raw_data/2006/council_csvs/output_", i, ".csv")), skip=1)%>%
+     dplyr::select(dplyr::starts_with("Data"))%>%
+     dplyr::mutate(DH_GEOGRAPHY_NAME=i)
+   
+   all_df <- rbind(all_df, df)
+   print(i)
+ }
+ 
+ 
+ asc06 <- all_df %>% 
+   dplyr::filter(Data.item.description=="residents aged 65 and over in own provision residential placements"|
+                   Data.item.description=="residents aged 65 and over in residential placements provided by others" )%>%
+   dplyr::distinct()%>%
+   dplyr::mutate(SupportSetting="Residential",
+                 ActivityProvision = ifelse(Data.item.description=="residents aged 65 and over in own provision residential placements", "In House",
+                                            "External"),
+                 year=2006,
+                 ITEMVALUE=as.numeric(Data.item.data))%>%
+   dplyr::group_by(DH_GEOGRAPHY_NAME,SupportSetting) %>%
+   dplyr::mutate(percent_sector = ITEMVALUE /(ITEMVALUE[ActivityProvision == "External"]+ITEMVALUE[ActivityProvision == "In House"])*100) %>%
+   dplyr::ungroup()
+ 
+ 
 
 
+#2002
+ 
+ excel_path <- "C:\\Users\\benjamin.goodair\\OneDrive - Nexus365\\Documents\\GitHub\\adults_social_care_data\\Raw_data\\2002\\spend.xls"
+ 
+ excel_sheets <- excel_sheets(excel_path)
+ 
+ for (sheet_name in excel_sheets) {
+   df <- read_excel(excel_path, sheet = sheet_name, col_types = NULL)
+   
+   csv_path <- paste0("C:\\Users\\benjamin.goodair\\OneDrive - Nexus365\\Documents\\GitHub\\adults_social_care_data\\Raw_data\\2002\\spend_csvs\\output_", sheet_name, ".csv")
+   
+   write.csv(df, file = csv_path, row.names = FALSE)
+ }
+ 
+ write.csv(excel_sheets, file = "C:\\Users\\benjamin.goodair\\OneDrive - Nexus365\\Documents\\GitHub\\adults_social_care_data\\Raw_data\\2002\\spend_csvs\\sheet_names.csv", row.names = FALSE)
+ 
+ 
+ 
+ 
+ #2003
+ 
+ excel_path <- "C:\\Users\\benjamin.goodair\\OneDrive - Nexus365\\Documents\\GitHub\\adults_social_care_data\\Raw_data\\2003\\spend.xls"
+ 
+ excel_sheets <- excel_sheets(excel_path)
+ 
+ for (sheet_name in excel_sheets) {
+   df <- read_excel(excel_path, sheet = sheet_name, col_types = NULL)
+   
+   csv_path <- paste0("C:\\Users\\benjamin.goodair\\OneDrive - Nexus365\\Documents\\GitHub\\adults_social_care_data\\Raw_data\\2003\\spend_csvs\\output_", sheet_name, ".csv")
+   
+   write.csv(df, file = csv_path, row.names = FALSE)
+ }
+ 
+ write.csv(excel_sheets, file = "C:\\Users\\benjamin.goodair\\OneDrive - Nexus365\\Documents\\GitHub\\adults_social_care_data\\Raw_data\\2003\\spend_csvs\\sheet_names.csv", row.names = FALSE)
+ 
+ #2004
+ 
+ excel_path <- "C:\\Users\\benjamin.goodair\\OneDrive - Nexus365\\Documents\\GitHub\\adults_social_care_data\\Raw_data\\2004\\spend.xls"
+ 
+ excel_sheets <- excel_sheets(excel_path)
+ 
+ for (sheet_name in excel_sheets) {
+   df <- read_excel(excel_path, sheet = sheet_name, col_types = NULL)
+   
+   csv_path <- paste0("C:\\Users\\benjamin.goodair\\OneDrive - Nexus365\\Documents\\GitHub\\adults_social_care_data\\Raw_data\\2004\\spend_csvs\\output_", sheet_name, ".csv")
+   
+   write.csv(df, file = csv_path, row.names = FALSE)
+ }
+ 
+ write.csv(excel_sheets, file = "C:\\Users\\benjamin.goodair\\OneDrive - Nexus365\\Documents\\GitHub\\adults_social_care_data\\Raw_data\\2004\\spend_csvs\\sheet_names.csv", row.names = FALSE)
+ 
+ 
+ 
 
